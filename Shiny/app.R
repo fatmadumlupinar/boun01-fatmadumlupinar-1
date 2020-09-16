b24e02429c5bb33fff8ef26d088e7547594fcb4b
Fatma Nur Dumlupınar <fatmanur.dumlupinar@boun.edu.tr>
2020-09-16 22:59:06 +0300

    Update app.R

--- Shiny/app.R
+++ Shiny/app.R
@@ -1,51 +1,32 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
-install.packages("ggplot2movies")
-library()
-library(shiny)
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+pti <- c("shiny","tidyverse","ggplot2movies")
+pti <- pti[!(pti %in% installed.packages())]
+if(length(pti)>0){
+  install.packages(pti)
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+##########
+### Shiny starter code
+##########
+library(shiny)
+library(tidyverse)
+library(ggplot2movies)
+
+# Set randomness seed
+set.seed(61)
+# Prepare data
+shiny_movie_set <- 
+movies %>% 
+  filter(year >= 2000) %>%
+  select(title,year,length,rating,votes,Action:Short) %>% 
+  gather(genre,value,Action:Short) %>% 
+  filter(value == 1) %>% 
+  select(-value)
+
+# Get genre list
+genres <- 
+shiny_movie_set %>% 
+  distinct(genre) %>% 
+  unlist(.)
+
+names(genres) <- NULL
